579589984cd9f759deb982b855cc1b2492a95350
Gustav0Furtad0 <gufurtado02@gmail.com>
2022-10-05 14:11:51 -0300

    leitura planilha auxiliar

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import openpyxl as opx;
-import pyexcel as opexcel;
+#import pyexcel as opexcel;
 
-arquivoFinal = opx.load_workbook(filename='intensBranetJF.xlsx');
-planilhaFinal = arquivoFinal['MEDICAMENTOS'];
+arquivoFinal = opx.load_workbook(filename='itensBranetJF.xlsx')
+planilhaFinal = arquivoFinal['MEDICAMENTOS']
 
-planilhaAuxiliar = input('Digite o nome da planilha a ser inserida')
-planilhaAuxiliar = opx.load_workbook(filename='planilha'
+arquivoAuxiliar = input('Digite o nome da planilha a ser inserida com seu formato')
+arquivoAuxiliar = opx.load_workbook(filename=arquivoAuxiliar)
+planilhaAuxiliar = arquivoAuxiliar["CMM UBS's"]
 
-for i in range(3, 464):
-    val = planilhaFinal.cell(row=i, column=2).value.split()
+i = 3
+while planilhaAuxiliar.cell(row=i, column=2).value != None:
+    val = [i, planilhaAuxiliar.cell(row=i, column=1).value, planilhaAuxiliar.cell(row=i, column=2).value]
     print(val)
+    i += 1
 
 # arquivofinal.save(filename='relatorios.xlsx')
